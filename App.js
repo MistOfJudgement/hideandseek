0d0149a5104cb8ef0c65246929ded75df88cffc8
Tushar <stuffcopresident@gmail.com>
2024-04-07 12:17:55 -0400

    Tested Screens and Dialogu input

--- App.js
+++ App.js
@@ -1,13 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+import Model from "react-native-modal"
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+import {useState} from "react";
+import Dialog from "react-native-dialog"
+function TitleScreen({navigation}) {
+
+    const [codeModalVisible, setCodeModalVisible] = useState(false);
+    return (
+        <View
+            style={styles.container}
+        >
+            <Text>TitleScreen</Text>
+
+            <Button
+                title="Create Lobby"
+                onPress={() => navigation.navigate("Lobby")}
+            />
+            <Button
+                title="Join Lobby"
+                onPress={()=> setCodeModalVisible(true)}
+            />
+            <Dialog.Container visible={codeModalVisible}>
+                <Dialog.Title>Enter Code</Dialog.Title>
+                <Dialog.Input>adsf</Dialog.Input>
+                <Dialog.Button>Cancel</Dialog.Button>
+                <Dialog.Button>Enter</Dialog.Button>
+            </Dialog.Container>
+        </View>
+    );
+}
+
+function LobbyScreen({navigation}) {
+
+    return (
+        <View>
+            <Text>Lobby</Text>
+        </View>
+    )
+}
 
 export default function App() {
+    const stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>New Text</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <stack.Navigator>
+            <stack.Screen name="Title" component={TitleScreen}/>
+            <stack.Screen name="Lobby" component={LobbyScreen}
+
+            />
+        </stack.Navigator>
+      </NavigationContainer>
   );
 }
 
